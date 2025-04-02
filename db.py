f166db4dd216bfa5d39e7558212331db90ea13f8
BaronBrabant <benny@saranne.be>
2025-04-02 18:48:35 +0100

    adds the script to run to create and spinup the docker milvus container, adds the logic to retrieve all non embedded images

--- db.py
+++ db.py
@@ -8,10 +8,9 @@ def init_db(db_path="labels.db"):
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS images (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            md5 TEXT PRIMARY KEY,
             image_path TEXT UNIQUE,
-            label TEXT,
-            md5 TEXT
+            label TEXT
         )
     ''')
     conn.commit()
@@ -24,7 +23,7 @@ def label_images(directory, model, conn):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.heic')):
             full_path = os.path.join(directory, filename)
             # Check if this image is already labeled
-            cursor.execute("SELECT id FROM images WHERE image_path = ?", (full_path,))
+            cursor.execute("SELECT md5 FROM images WHERE image_path = ?", (full_path,))
             if cursor.fetchone() is None:
                 # Call the model to label the image
                 description = model.imageQuery(full_path, "Describe what is in this image in one sentence.")
@@ -34,8 +33,8 @@ def label_images(directory, model, conn):
                         file_hash = md5(file_data).hexdigest()
 
                     cursor.execute(
-                        "INSERT INTO images (image_path, label, md5) VALUES (?, ?, ?)",
-                        (full_path, description, file_hash)
+                        "INSERT INTO images (md5, image_path, label) VALUES (?, ?, ?)",
+                        (file_hash, full_path, description)
                     )
                     conn.commit()
                     print(f"Labeled {filename}: {description}")
@@ -43,5 +42,35 @@ def label_images(directory, model, conn):
                     print(f"Failed to label {filename}")
             else:
                 print(f"Already labeled {filename}")
+
+
+
+def retrieve_images(conn, hashes):
+    """Retrieves the images in the SQL DB and checks if they already exist in Milvus, returns the non-existent ones for embedding."""
+    cursor = conn.cursor()
+    
+    placeholders = ','.join('?' for _ in hashes)
+    query = f"SELECT * FROM images WHERE md5 NOT IN ({placeholders})"
+    cursor.execute(query, hashes)
+    
+    infos = cursor.fetchall()
+    if not infos:
+        raise Exception("No images to label")
+    else:
+        print(infos)
+        return infos
+
+
+class ImageInformation():
+
+    def __init__(self, md5, path, description):
+        self.md5 = md5
+        self.path = path
+        self.description = description
+
+
+    def get_info(self):
+
+        return self.md5, self.path, self.description
 
 
